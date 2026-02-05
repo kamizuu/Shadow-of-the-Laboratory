abd59d79a53b71ae3ec2cc243abded68d2f3118f
jaydeyhs <jaydeyhs@gmail.com>
2026-02-05 17:08:09 -0600

    animated movement
    animate player when moved around

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,10 @@
 class_name Player extends CharacterBody2D
 
 
+
 var move_speed : float = 500.0
 
+@onready var sprite : AnimatedSprite2D = $Sprite
 
 func _ready():
 	pass
@@ -13,10 +15,32 @@ func _process( _delta ):
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 
+<<<<<<< Updated upstream
 	velocity = direction * move_speed
+=======
+    velocity = direction * move_speed
+
+    if direction != Vector2.ZERO:
+        if abs(direction.x) > abs(direction.y):
+            if direction.x > 0:
+                $AnimatedSprite2D.animation = "look right"
+            else:
+                $AnimatedSprite2D.animation = "look left"
+        else:
+            if direction.y > 0:
+                $AnimatedSprite2D.animation = "look down"
+            else:
+                $AnimatedSprite2D.animation = "look up"
+    
+>>>>>>> Stashed changes
 
 	pass
 
 
 func _physics_process( _delta ):
+<<<<<<< Updated upstream
 	move_and_slide()
+=======
+    move_and_slide()
+    
+>>>>>>> Stashed changes
